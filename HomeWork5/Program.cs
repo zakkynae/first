354299f8a629449b53a795b21a928d849e9612ea
Anastasia Soboleva <flamrosae@yandex.ru>
2022-04-25 14:45:25 +0500

    AllDirectories

--- HomeWork5/Program.cs
+++ HomeWork5/Program.cs
@@ -1,6 +1,21 @@
 ﻿using ConsoleTables;
 class Program
 {
+    private static void GetArraysDataAllDirectories(DirectoryInfo directory, out long[] arrayLengthFiles, out DateTime[] arrayDateCreationFile, out string[] arrayExtensionFiles)
+    {
+        var listFiles = directory.GetFiles("*.*", SearchOption.AllDirectories);
+        arrayLengthFiles = new long[listFiles.Length];
+        arrayDateCreationFile = new DateTime[listFiles.Length];
+        arrayExtensionFiles = new string[listFiles.Length];
+        for (int i = 0; i < listFiles.Length; i++)
+        {
+            arrayLengthFiles[i] = listFiles[i].Length;
+            arrayDateCreationFile[i] = listFiles[i].CreationTime;
+            arrayExtensionFiles[i] = listFiles[i].Extension;
+        }
+        Array.Sort(arrayLengthFiles);
+        Array.Sort(arrayDateCreationFile);
+    }
     private static void GetArraysData(DirectoryInfo directory, out long[] arrayLengthFiles, out DateTime[] arrayDateCreationFile, out string[] arrayExtensionFiles)
     {
         var listFiles = directory.GetFiles();
@@ -25,7 +40,6 @@ class Program
             amountExtensionFiles[extension]++;
         }
         amountExtensionFiles = amountExtensionFiles.OrderByDescending(pair => pair.Value).ToDictionary(pair => pair.Key, pair => pair.Value);
-
         return amountExtensionFiles.Keys.ToArray();
     }
     private static ConsoleTable GetTable(string[] arrayExtensionFiles, long[] arrayLengthFiles, DateTime[] arrayDateCreationFile)
@@ -33,7 +47,7 @@ class Program
         var table = new ConsoleTable("The largest files", "The most popular extensions", "The oldest files");
         for (int i = 0; i < 3; i++)
         {
-            table.AddRow(arrayLengthFiles[i], arrayExtensionFiles[i], arrayDateCreationFile[i]);
+            table.AddRow(arrayLengthFiles[arrayLengthFiles.Length - 1 - i], arrayExtensionFiles[i], arrayDateCreationFile[i]);
         }
         return table;
     }
@@ -42,13 +56,18 @@ class Program
         Console.Write("Введите директорию: ");
         var path = String.Format($@"{Console.ReadLine()}");
         var directory = new DirectoryInfo(path);
+        string[] arrayExtensionFilesAllDirectories;
+        long[] arrayLengthFilesAllDirectories;
+        DateTime[] arrayDateCreationFileAllDirectories;
+        GetArraysDataAllDirectories(directory, out arrayLengthFilesAllDirectories, out arrayDateCreationFileAllDirectories, out arrayExtensionFilesAllDirectories);
+        var firstTable = GetTable(GetSortedArrayExtension(arrayExtensionFilesAllDirectories), arrayLengthFilesAllDirectories, arrayDateCreationFileAllDirectories);
+        Console.WriteLine("");
+        firstTable.Write();
         string[] arrayExtensionFiles;
         long[] arrayLengthFiles;
         DateTime[] arrayDateCreationFile;
-
         GetArraysData(directory, out arrayLengthFiles, out arrayDateCreationFile, out arrayExtensionFiles);
-        GetSortedArrayExtension(arrayExtensionFiles);
-        var table = GetTable(arrayExtensionFiles, arrayLengthFiles, arrayDateCreationFile);
-        table.Write();
+        var secondTable = GetTable(GetSortedArrayExtension(arrayExtensionFiles), arrayLengthFiles, arrayDateCreationFile);
+        secondTable.Write();
     }
 }
