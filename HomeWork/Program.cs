93cc4404db1cd39dad8217af7535cbff10ddc875
Anastasia Soboleva <flamrosae@yandex.ru>
2022-05-01 18:10:36 +0500

    benchmarkdotnet

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -1,59 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
 
 namespace HomeWork
-{
-    internal class Program
+{   
+    [MemoryDiagnoser]
+    public class Logic
     {
-        public static string GetFirstTriangle(int row)
+        [Params(10, 100, 500)]
+        public  int Row { get; set; }
+        [Benchmark]
+        public  string GetTriangleStringBuilder()
         {
+            var row = Row;
             var triangle = new StringBuilder("");
-            for(int i = 1; i < row + 1; i++)
+            for (int i = 1; i < row + 1; i++)
             {
                 triangle.Append(new String('*', i));
                 triangle.Append('\n');
             }
             return triangle.ToString();
         }
-        public static List<string> GetSecondTriangle(int row)
+        [Benchmark]
+        public  List<string> GetTriangleListString()
         {
-            var elementSecondTriangle = "";
-            for(var i = 0; i < row-3; i++)
-                elementSecondTriangle += " ";
-            var secondTriangle = new List<string>() {(elementSecondTriangle += "*")};
-            for(var i = 0; i < row - 3; i++)
-            {
-                elementSecondTriangle = elementSecondTriangle.Substring(1) + "**";
-                secondTriangle.Add(elementSecondTriangle);
-            }
-            return secondTriangle;
+            var row = Row;
+            var elementFirstTriangle = "";
+            var firstTriangle = new List<string>() { elementFirstTriangle };
+            for (var i = 0; i < row; i++)
+                firstTriangle.Add(elementFirstTriangle += "*");
+            return firstTriangle;
         }
-
-        public static void PrintTriangle(List<string> triangle)
-        {
-            foreach(var element in triangle)
-                Console.WriteLine(element);
-        }
-        public static void PrintTriangle(string triangle)
-        {
-            foreach (var element in triangle)
-                Console.Write(element);
-        }
-
+    }
+    internal class Program
+    {
         static void Main(string[] args)
         {
-            Console.Write("Введите кол-во строк треугольника: ");
-
-            int row;
-            while(!(int.TryParse(Console.ReadLine(), out row)) || row < 3)
-            {
-                Console.WriteLine("Введено неверное кол-во строк треугольника. Попробуйте еще раз.");
-            }
-            PrintTriangle(GetFirstTriangle(row));
-            //GetFirstTriangle(row);
-            Console.WriteLine();
-            PrintTriangle(GetSecondTriangle(row));
+            BenchmarkRunner.Run<Logic>();
         }
     }
 }
